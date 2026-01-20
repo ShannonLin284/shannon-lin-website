a7ff742475c3a974915fe46371f59c2e4630477f
Shannon Lin <Shannonlin24@gmail.com>
2026-01-20 13:14:43 -0500

    Update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repo = "shannon-lin-website";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: { unoptimized: true },
 };
 
