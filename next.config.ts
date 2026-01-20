c66629607905edf5efecb571c7429afd47ef222a
Shannon Lin <Shannonlin24@gmail.com>
2026-01-20 16:51:22 -0500

    Deploy on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const repo = "shannon-lin-website";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
-  images: { unoptimized: true },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
